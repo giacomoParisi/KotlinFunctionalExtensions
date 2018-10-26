5a899b02f4be8fc0e10486e5e9441e31db63d4e2
Giacomo Parisi <giacomo@balzo.eu>
2018-10-26 15:42:31 +0200

    Add project

---
+++ arrow/src/main/java/com/giacomoparisi/kotlinfunctionalextensions/arrow/OptionExt.kt
@@ -0,0 +1,54 @@
+package com.giacomoparisi.kotlinfunctionalextensions.arrow
+
+import arrow.core.None
+import arrow.core.Option
+import arrow.core.Some
+import com.giacomoparisi.kotlinfunctionalextensions.core.ifTrue
+
+/**
+ * Execute the $action function if the option value is Some.
+ *
+ * @author Giacomo Parisi
+ * @param action The function to execute when the option value is Some
+ */
+fun <T> Option<T>.ifSome(action: (T) -> Unit) {
+    when (this) {
+        is Some -> action(this.t)
+    }
+}
+
+/**
+ * Execute the $action function if the option value is None.
+ *
+ * @author Giacomo Parisi
+ * @param action The function to execute when the option value is None
+ */
+fun <T> Option<T>.ifNone(action: () -> Unit) {
+    this.isEmpty().ifTrue { action() }
+}
+
+/**
+ * Execute the $ifSome function if the option value is Some.
+ * Otherwise execute the $ifNone function.
+ *
+ * @author Giacomo Parisi
+ * @param ifSome The function to execute when the option value is Some
+ * @param ifNone The function to execute when the option value is None
+ */
+fun <T> Option<T>.Match(ifSome: (T) -> Unit, ifNone: () -> Unit) {
+    when (this) {
+        is Some -> ifSome(this.t)
+        is None -> ifNone()
+    }
+}
+
+/**
+ * Fold the option and return a string, using the $action if the option is Some
+ * Or empty string if the option is None
+ *
+ * @author Giacomo Parisi
+ * @param action The function to execute when the option value is Some
+ * @return Return the string folded value
+ */
+fun <T> Option<T>.foldToString(action: (T) -> String): String =
+        this.fold({ "" }) { action(it) }
---
+++ core/src/main/java/com/giacomoparisi/kotlinfunctionalextensions/core/BooleanExt.kt
@@ -0,0 +1,49 @@
+package com.giacomoparisi.kotlinfunctionalextensions.core
+
+/**
+ * Returns the T object of $ifTrue if the value is true.
+ * Otherwise return the the T object of $ifFalse.
+ *
+ * @author Giacomo Parisi
+ * @param ifTrue The object to return when the value is true
+ * @param ifFalse The object to return when the value is false
+ * @return The correct T object based on the value
+ */
+fun <T> Boolean.fold(ifFalse: T, ifTrue: T): T =
+        if (this) ifTrue else ifFalse
+
+/**
+ * Returns the T object from $ifTrue function if the value is true.
+ * Otherwise return the the T object from $ifFalse function.
+ *
+ * @author Giacomo Parisi
+ * @param ifTrue The function that return the T object when the value is true
+ * @param ifFalse The function that return the T object when the value is false
+ * @return The correct T object based on the value
+ */
+fun <T> Boolean.fold(ifFalse: () -> T, ifTrue: () -> T): T =
+        if (this) ifTrue() else ifFalse()
+
+/**
+ * Execute the $action function if the value is true.
+ *
+ * @author Giacomo Parisi
+ * @param action The function to execute when the value is true
+ * @return The source boolean value
+ */
+fun Boolean.ifTrue(action: () -> Unit): Boolean {
+    if (this) action()
+    return this
+}
+
+/**
+ * Execute the $action function if the value is false.
+ *
+ * @author Giacomo Parisi
+ * @param action The function to execute when the value is false
+ * @return The source boolean value
+ */
+fun Boolean.ifFalse(action: () -> Unit): Boolean {
+    if (!this) action()
+    return this
+}
