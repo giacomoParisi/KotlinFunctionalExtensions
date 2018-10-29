7ac8b8179e32aa746fc85458a238d3625eb7179a
Giacomo Parisi <giacomo@balzo.eu>
2018-10-29 17:49:33 +0100

    Fix try failure extensions

--- kotlin-functional-extensions-arrow/src/main/java/com/giacomoparisi/kotlin/functional/extensions/arrow/TryExt.kt
+++ kotlin-functional-extensions-arrow/src/main/java/com/giacomoparisi/kotlin/functional/extensions/arrow/TryExt.kt
@@ -3,7 +3,6 @@ package com.giacomoparisi.kotlin.functional.extensions.arrow
 import arrow.core.Failure
 import arrow.core.Success
 import arrow.core.Try
-import com.giacomoparisi.kotlin.functional.extensions.core.ifTrue
 
 /**
  * Execute the $action function if the try value is Success.
@@ -13,8 +12,7 @@ import com.giacomoparisi.kotlin.functional.extensions.core.ifTrue
  * @return The source try object
  */
 fun <T> Try<T>.ifSuccess(action: (T) -> Unit): Try<T> {
-    when (this
-        ) {
+    when (this) {
         is Success -> action(this.value)
     }
     return this
@@ -27,8 +25,10 @@ fun <T> Try<T>.ifSuccess(action: (T) -> Unit): Try<T> {
  * @param action The function to execute when the try value is Failure
  * @return The source try object
  */
-fun <T> Try<T>.ifFailure(action: () -> Unit): Try<T> {
-    this.isFailure().ifTrue { action() }
+fun <T> Try<T>.ifFailure(action: (throwable: Throwable) -> Unit): Try<T> {
+    when (this) {
+        is Failure -> action(this.exception)
+    }
     return this
 }
 
@@ -41,21 +41,21 @@ fun <T> Try<T>.ifFailure(action: () -> Unit): Try<T> {
  * @param ifFailure The function to execute when the try value is Failure
  * @return The source try object
  */
-fun <T> Try<T>.Match(ifSuccess: (T) -> Unit, ifFailure: () -> Unit): Try<T> {
+fun <T> Try<T>.Match(ifSuccess: (T) -> Unit, ifFailure: (throwable: Throwable) -> Unit): Try<T> {
     when (this) {
         is Success -> ifSuccess(this.value)
-        is Failure -> ifFailure()
+        is Failure -> ifFailure(this.exception)
     }
     return this
 }
 
 /**
  * Fold the try and return a string, using the $action if the try is Success
- * Or empty string if the try is Failure
+ * Or the message of the throwable if the try is Failure
  *
  * @author Giacomo Parisi
  * @param action The function to execute when the try value is Success
  * @return Return the string folded value
  */
-fun <T> Try<T>.foldToString(action: (T) -> String): String =
-        this.fold({ "" }) { action(it) }
+fun <T> Try<T>.foldToMessage(action: (T) -> String): String =
+        this.fold({ it.message.orEmpty() }) { action(it) }
