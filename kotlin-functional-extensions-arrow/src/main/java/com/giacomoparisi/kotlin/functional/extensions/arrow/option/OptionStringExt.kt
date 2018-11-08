378cd350630ca5ae436e642d64d8bcfef21c4310
Giacomo Parisi <giacomo@balzo.eu>
2018-11-08 14:48:40 +0100

    Added empty and blank conversion for option

--- kotlin-functional-extensions-arrow/src/main/java/com/giacomoparisi/kotlin/functional/extensions/arrow/option/OptionStringExt.kt
+++ kotlin-functional-extensions-arrow/src/main/java/com/giacomoparisi/kotlin/functional/extensions/arrow/option/OptionStringExt.kt
@@ -3,6 +3,7 @@ package com.giacomoparisi.kotlin.functional.extensions.arrow.option
 import arrow.core.None
 import arrow.core.Option
 import arrow.core.Some
+import arrow.core.some
 
 /**
  * Return the string value if the option is Some
@@ -16,3 +17,33 @@ fun Option<String>.getOrEmpty(): String =
             is Some -> this.t
             is None -> ""
         }
+
+/**
+ * Return an Option of the source string if the string is not empty
+ * Or None if the source string is empty
+ *
+ * @author Giacomo Parisi
+ * @return Return the Option of the string with None value if the string is empty
+ */
+fun String.emptyToNone(): Option<String> =
+        if(this.isEmpty()) None else this.some()
+
+/**
+ * Return an Option of the source string if the string is not blank
+ * Or None if the source string is blank
+ *
+ * @author Giacomo Parisi
+ * @return Return the Option of the string with None value if the string is blank
+ */
+fun String.blankToNone(): Option<String> =
+        if(this.isBlank()) None else this.some()
+
+/**
+ * Return an Option of the source string if the string is not empty and not blank
+ * Or None if the source string is empty or blank
+ *
+ * @author Giacomo Parisi
+ * @return Return the Option of the string with None value if the string is empty or blank
+ */
+fun String.emptyAndBlankToNone(): Option<String> =
+        if(this.isEmpty() || this.isBlank()) None else this.some()
