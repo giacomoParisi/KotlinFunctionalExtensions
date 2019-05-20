51eb4818ae3882b2e4b813e0dbb7c776a938beea
Giacomo Parisi <giacomo@balzo.eu>
2019-05-20 10:36:10 +0200

    Remove coroutine module

--- kotlin-functional-extensions-coroutines/src/main/java/com/giacomoparisi/kotlin/functional/extensions/coroutines/CoroutineScopeExt.kt
+++
@@ -1,9 +0,0 @@
-package com.giacomoparisi.kotlin.functional.extensions.coroutines
-
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-
-inline fun postOnUI(crossinline action: () -> Unit) {
-    CoroutineScope(Dispatchers.Main).launch { action() }
-}
