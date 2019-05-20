64500b9cb7276780cd6dccdbedb0c29189ab0dce
Giacomo Parisi <giacomo@balzo.eu>
2019-05-20 11:10:32 +0200

    Add configuration for bintray release

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,3 @@
-import kotlin.String
-
 /**
  * Find which updates are available by running
  *     `$ ./gradlew syncLibs`
@@ -7,30 +5,31 @@ import kotlin.String
  *
  * YOU are responsible for updating manually the dependency version. */
 object Versions {
-    const val androidx_core_core: String = "1.0.1" 
+    const val androidx_core_core: String = "1.0.2"
 
-    const val espresso_core: String = "3.1.0" 
+    const val espresso_core: String = "3.1.1"
 
-    const val androidx_test_runner: String = "1.1.0" 
+    const val androidx_test_runner: String = "1.1.1"
 
-    const val aapt2: String = "3.2.1-4818971" 
+    const val com_android_tools_build_gradle: String = "3.4.1"
 
-    const val com_android_tools_build_gradle: String = "3.2.1" 
+    const val lint_gradle: String = "26.4.1" 
 
-    const val lint_gradle: String = "26.2.1" 
+    const val android_maven_gradle_plugin: String = "2.1"
 
-    const val android_maven_gradle_plugin: String = "2.1" 
+    const val gradle_bintray_plugin: String = "1.8.4"
 
-    const val io_arrow_kt: String =
-            "0.8.0" // No update information. Is this dependency available on jcenter or mavenCentral?
+    const val io_arrow_kt: String = "0.8.0"
+    /* Could not resolve io.arrow-kt:arrow-annotations-processor:+.
+    .... */
 
     const val jmfayard_github_io_gradle_kotlin_dsl_libs_gradle_plugin: String = "0.2.6" 
 
-    const val junit: String = "4.12" 
+    const val junit: String = "4.12"
 
-    const val org_jetbrains_kotlin: String = "1.3.10" 
+    const val org_jetbrains_dokka: String = "0.9.18"
 
-    const val kotlinx_coroutines_core: String = "1.0.1" 
+    const val org_jetbrains_kotlin: String = "1.3.31" 
 
     /**
      *
@@ -38,12 +37,12 @@ object Versions {
      *      ./gradle/wrapper/gradle-wrapper.properties
      */
     object Gradle {
-        const val runningVersion: String = "4.10.2"
+        const val runningVersion: String = "5.4.1"
 
-        const val currentVersion: String = "4.10.2"
+        const val currentVersion: String = "5.4.1"
 
-        const val nightlyVersion: String = "5.1-20181114000030+0000"
+        const val nightlyVersion: String = "5.6-20190520000026+0000"
 
-        const val releaseCandidate: String = "5.0-rc-2"
+        const val releaseCandidate: String = ""
     }
 }
