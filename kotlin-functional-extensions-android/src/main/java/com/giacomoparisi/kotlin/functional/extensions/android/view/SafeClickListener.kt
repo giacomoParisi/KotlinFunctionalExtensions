a6e008ea332fccf5164a9fdc5ea990b434f1eff0
Giacomo Parisi <giacomo@balzo.eu>
2019-10-31 16:21:05 +0100

    Add safe click listener

---
+++ kotlin-functional-extensions-android/src/main/java/com/giacomoparisi/kotlin/functional/extensions/android/view/SafeClickListener.kt
@@ -0,0 +1,25 @@
+package com.giacomoparisi.kotlin.functional.extensions.android.view
+
+import android.os.SystemClock
+import android.view.View
+
+class SafeClickListener(
+        private var defaultInterval: Int,
+        private val onSafeCLick: (View) -> Unit
+) : View.OnClickListener {
+    private var lastTimeClicked: Long = 0
+    override fun onClick(v: View) {
+        if (SystemClock.elapsedRealtime() - lastTimeClicked < defaultInterval) {
+            return
+        }
+        lastTimeClicked = SystemClock.elapsedRealtime()
+        onSafeCLick(v)
+    }
+}
+
+fun View.setSafeOnClickListener(interval: Int = 1000, onSafeClick: (View) -> Unit) {
+    val safeClickListener = SafeClickListener(interval) {
+        onSafeClick(it)
+    }
+    setOnClickListener(safeClickListener)
+}
