544368f284336b9dea6a84c3af6d0c309a9552f9
Giacomo Parisi <giacomo@balzo.eu>
2019-11-04 11:58:53 +0100

    Update dimensions ext to return int instead of float

--- kotlin-functional-extensions-android/src/main/java/com/giacomoparisi/kotlin/functional/extensions/android/view/DimensionsExt.kt
+++ kotlin-functional-extensions-android/src/main/java/com/giacomoparisi/kotlin/functional/extensions/android/view/DimensionsExt.kt
@@ -2,28 +2,51 @@ package com.giacomoparisi.kotlin.functional.extensions.android.view
 
 import android.content.res.Resources
 import android.util.DisplayMetrics
+import kotlin.math.roundToInt
 
 
 /**
  * This method converts dp unit to equivalent pixels, depending on device density.
  * @author Giacomo Parisi
- * @return A float value to represent px equivalent to dp depending on device density
+ * @return A int value to represent px equivalent to dp depending on device density
  */
-fun Float.dpToPx(): Float = this * (
-        Resources.getSystem()
-                .displayMetrics
-                .densityDpi
-                .toFloat()
-                / DisplayMetrics.DENSITY_DEFAULT)
+fun Float.dpToPx(): Int =
+        (this * (
+                Resources.getSystem()
+                        .displayMetrics
+                        .densityDpi
+                        .toFloat()
+                        / DisplayMetrics.DENSITY_DEFAULT))
+                .roundToInt()
+
+fun Int.dpToPx(): Int =
+        (this * (
+                Resources.getSystem()
+                        .displayMetrics
+                        .densityDpi
+                        .toFloat()
+                        / DisplayMetrics.DENSITY_DEFAULT))
+                .roundToInt()
 
 /**
  * This method converts device specific pixels to density independent pixels.
  * @author Giacomo Parisi
- * @return A float value to represent dp equivalent to px value
+ * @return A int value to represent dp equivalent to px value
  */
-fun Float.pxToDp(): Float = this / (
-        Resources.getSystem()
-                .displayMetrics
-                .densityDpi
-                .toFloat()
-                / DisplayMetrics.DENSITY_DEFAULT)
+fun Float.pxToDp(): Int =
+        (this / (
+                Resources.getSystem()
+                        .displayMetrics
+                        .densityDpi
+                        .toFloat()
+                        / DisplayMetrics.DENSITY_DEFAULT))
+                .roundToInt()
+
+fun Int.pxToDp(): Int =
+        (this / (
+                Resources.getSystem()
+                        .displayMetrics
+                        .densityDpi
+                        .toFloat()
+                        / DisplayMetrics.DENSITY_DEFAULT))
+                .roundToInt()
